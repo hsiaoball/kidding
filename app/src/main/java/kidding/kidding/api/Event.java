f2a833e13e882a7a9463d1aee617ce38a5b796b8
ChiYo Hsiao <hsiaoball@gmail.com>
2015-02-03 21:57:41 -0800

    add api dir

---
+++ app/src/main/java/kidding/kidding/api/Event.java
@@ -0,0 +1,33 @@
+package kidding.kidding.api;
+
+import android.graphics.Bitmap;
+
+import java.util.Date;
+
+/**
+ * Created by ChiYo Hsiao on 2/2/2015.
+ */
+public class Event {
+    public String mTopic;
+    public Date mTime;
+    public int mCost;
+    public String mContent;
+    public Bitmap mImage;
+    public String mAddress;
+    public String mCity;
+    public String mState;
+    public String mZip;
+    public String mOrganizer;
+    public String mWebSite;
+    public String mParking;
+    public String mAge;
+    public String mPhone;
+
+    public Event(String topic, Date time, int cost, String content){
+        mTopic=topic;
+        mTime=time;
+        mCost=cost;
+        mContent=content;
+        //mImage=image;
+    }
+}
